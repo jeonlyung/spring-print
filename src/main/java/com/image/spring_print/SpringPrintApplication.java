3636a5ac7e25a9c281ed373a9efbfe9864c3b1e6
jeonlyung <bindongx2@naver.com>
2025-01-17 13:03:16 +0900

    first commit

---
+++ src/main/java/com/image/spring_print/SpringPrintApplication.java
@@ -0,0 +1,13 @@
+package com.image.spring_print;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPrintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/image/spring_print/SpringPrintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.image.spring_print;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPrintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
